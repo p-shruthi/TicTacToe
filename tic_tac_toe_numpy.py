6849e9b7d6238ea03a8a4810dcad6c61e23a404e
p-shruthi <66623373+p-shruthi@users.noreply.github.com>
2020-06-26 23:29:38 -0400

    Add files via upload

--- tic_tac_toe_numpy.py
+++ tic_tac_toe_numpy.py
@@ -4,10 +4,11 @@ def tic_tac_toe():
     board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     valid = False
     win_commbinations = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-    def draw():
+    def display_board():
         print(board[0], board[1], board[2])
         print(board[3], board[4], board[5])
         print(board[6], board[7], board[8])
+        print()
 
     def handle_turn():
         position = int(input())
@@ -49,14 +50,14 @@ def tic_tac_toe():
                 return True
 
     while not valid:
-        draw()
+        display_board()
         valid = check_board()
         if valid == True:
             break
         print("Player 1 choose where to place a cross")
         handle_turn()
         print()
-        draw()
+        display_board()
         valid = check_board()
         if valid == True:
             break
